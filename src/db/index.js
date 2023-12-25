e3708d6be2e77fff53f0a5443cd6d4ee4dc2051f
sahilbhise09 <sahilbhise09@gmail.com>
2023-12-26 03:14:17 +0530

    Connect To MongoDB Compass

---
+++ src/db/index.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017'}/${DB_NAME}`)
+        console.log(`\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.log("MONGODB connection error ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants';
+// import express from 'express'
+// const app = express();
+
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`process.env.${MONGODB_URL}/${DB_NAME}`);
+//         app.on("error", ()=>{
+//             console.log("Error: ", error);
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch(error){
+//         console.log("error: ", error );
+//         throw error
+//     }
+// })()
